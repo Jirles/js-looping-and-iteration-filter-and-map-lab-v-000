5d89323166a476d8ba3bca6c52c4b133852ae025
Emily Jirles <emily.jirles@gmail.com>
2018-07-11 21:20:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function driversWithRevenueOver(collection, num){
 };
 
 function driversNamesWithRevenueOver(collection, num){
-  return collection.filter(driver => driver.name if driver.revenue > num);
+  return collection.filter(driver => driver.revenue > num);
 };
